b1e53fffdda3db9d8ff992290758405015990dc1
Martin Burolla <mburolla@xpanxion.com>
2023-05-26 14:41:22 -0400

    update

--- stu0/app.js
+++ stu0/app.js
@@ -3,6 +3,14 @@ var stu0;
 (function (stu0) {
     const main = () => {
         console.log("Hello World");
+        // Cannot assign a string to a number because x has been assigned 
+        // a number type by the TypeScript compiler.
+        let x = 5150;
+        // Cannot assign a string to a number because y has been assigned 
+        // a number type by the developer.
+        let y = 5150;
+        // x = "test" // Error
+        // y = "test" // Error
     };
     main();
 })(stu0 || (stu0 = {}));
--- stu0/app.ts
+++ stu0/app.ts
@@ -1,7 +1,18 @@
 namespace stu0 {
     const main = () => {
         console.log("Hello World")
+
+        // Cannot assign a string to a number because x has been assigned 
+        // a number type by the TypeScript compiler.
+        let x = 5150 
+  
+        // Cannot assign a string to a number because y has been assigned 
+        // a number type by the developer.
+        let y: number = 5150 
+
+        // x = "test" // Error
+        // y = "test" // Error
     }
-    
+
     main()
 }
