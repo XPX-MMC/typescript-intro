c0c646fb38fdb392826dfb5cc4e851fc20a947b9
Martin Burolla <mburolla@xpanxion.com>
2023-05-31 14:36:31 -0400

    update

--- Interfaces-Abstract-Classes.md
+++ Interfaces-Abstract-Classes.md
@@ -2,15 +2,61 @@
 Update `interfaces-abstract-classes` in your student directory for all exercises.
 
 # Ex 1
+Update `ex1()` to instantiate a `Calculator` object.  The `Calculator` object must implement the `Calculateble` interface as defined here:
 
+```
+    interface Calculateble {
+       add(num1: number, num2: number): number
+       subtract(num1: number, num2: number): number
+       multiply(num1: number, num2: number): number
+       divide(num1: number, num2: number): number
+    }
+```
+
+Example usage:
+```
+const calculator = new Calculator()
+console.log(calculator.add(1, 2))
+console.log(calculator.subtract(4, 2))
+console.log(calculator.multiply(4, 3))
+console.log(calculator.divide(10, 2))
+
+```
+
+Output:
+```
+3
+2
+12
+5
+```
 
 # Ex 2
+Update `ex2()` to to instantiate a `CatDataAccess()` object.  This class implements an interface that contains one public method called `getCat()` which returns a `Cat` object.
 
+Example usage:
+```
+const catDataAccess = new CatDataAccess()
+const cat = catDataAccess.getCat()
+console.log(cat)
+```
+
+Output:
+```
+Cat { name: 'Fluffy', age: 2 }
+```
 
 # Ex 3
+Update `ex3()` to instantiate a `Square` object.  The Square object extends the abstract `Shape` class.  As a reference these items have been included in `interfaces-abstract-classes.ts`
 
+Example usage:
+```
+const square = new Square('blue', 5)
+console.log(square.getArea())
 
-# Ex 4
+```
 
-
-# Ex 5
+Output:
+```
+25
+```
--- stu0/interfaces-abstract-classes.ts
+++ stu0/interfaces-abstract-classes.ts
@@ -1,31 +1,115 @@
 namespace stu0_iac {
+
+    abstract class Shape {
+        protected color: string;
+      
+        constructor(color: string) {
+          this.color = color;
+        }
+      
+        abstract getArea(): number;
+      
+        getColor(): string {
+          return this.color;
+        }
+    }
+
+    class Circle extends Shape {
+        private radius: number;
+      
+        constructor(color: string, radius: number) {
+          super(color);
+          this.radius = radius;
+        }
+      
+        getArea(): number {
+          return Math.PI * Math.pow(this.radius, 2);
+        }
+    }
+
     const ex1 = () => {
         console.log('Exercise 1')
-
-    }
+        const calculator = new Calculator()
+        console.log(calculator.add(1, 2))
+        console.log(calculator.subtract(4, 2))
+        console.log(calculator.multiply(4, 3))
+        console.log(calculator.divide(10, 2))
+    }   
 
     const ex2 = () => {
         console.log('Exercise 2')
-
+        const catDataAccess = new CatDataAccess()
+        const cat = catDataAccess.getCat()
+        console.log(cat)
     }
 
     const ex3 = () => {
         console.log('Exercise 3')
-
+        const square = new Square('blue', 5)
+        console.log(square.getArea())
     }
 
-    const ex4 = () => {
-        console.log('Exercise 4')
+    //
+    // YOUR CODE HERE
+    //
 
+    interface Calculateble {
+       add(num1: number, num2: number): number
+       subtract(num1: number, num2: number): number
+       multiply(num1: number, num2: number): number
+       divide(num1: number, num2: number): number
     }
 
-    const ex5 = () => {
-        console.log('Exercise 5')
- 
+    class Calculator implements Calculateble {
+        add(num1: number, num2: number): number {
+            return num1 + num2
+        }
+        subtract(num1: number, num2: number): number {
+            return num1 - num2
+        }
+        multiply(num1: number, num2: number): number {
+            return num1 * num2
+        }
+        divide(num1: number, num2: number): number {
+            return num1 / num2
+        }
+    }
+
+    interface DataAccessable {
+        getCat(): Cat
+    }
+
+    class Cat {
+        public name: string
+        public age: number
+
+        constructor(name: string, age: number) {
+            this.name = name
+            this.age = age
+        }
+    }
+
+    class CatDataAccess implements DataAccessable {
+        getCat(): Cat {
+            return new Cat('Fluffy', 2)
+        }
+    }
+
+    class Square extends Shape {
+        private length: number;
+      
+        constructor(color: string, length: number) {
+          super(color);
+          this.length = length;
+        }
+      
+        getArea(): number {
+          return this.length * this.length;
+        }
     }
 
     const main = () : void => {
-        ex1() // Change this to ex2(), ex3(), ex4(), or ex5() to run the desired exercise.
+        ex3() // Change this to ex2(), ex3().
     }
 
     main()
